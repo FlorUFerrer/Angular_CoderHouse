a77480578bd9dcae2d40390b549c0c03f4a9a3ff
Florencia Urrejola Ferrer <florurre@gmail.com>
2022-11-17 20:56:20 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+
+//Componentes
+import { NavBarComponent } from './views/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: NavBarComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
